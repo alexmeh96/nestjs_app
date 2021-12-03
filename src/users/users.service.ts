250da93ab0dd078640adee908fcd174b22d078ca
alexmeh <alex.meh96@mail.ru>
2021-12-03 22:08:33 +0300

    add auth

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,11 +14,17 @@ export class UsersService {
     const user = await this.userRepository.create(dto);
     const role = await this.roleService.getRoleByValue("USER")
     await user.$set('roles', [role.id])  //  перезаписать поле roles в бд
+    user.roles = [role]
     return user;
   }
 
   async  getAllUsers() {
     const users = await this.userRepository.findAll({include: {all: true}});  // {include: {all: true}} - подтягивать все поля из других таблиц
     return users;
+  }
+
+  async getUsersByEmail(email: string) {
+    const user = await this.userRepository.findOne({where: {email}, include: {all: true}})
+    return user;
   }
 }
