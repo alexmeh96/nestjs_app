7fe83afbd61c0a58eea775122e8772389d26dd52
alexmeh <alex.meh96@mail.ru>
2021-12-04 12:11:09 +0300

    add post with model oneToMany

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post-dto';
+
+@Controller('posts')
+export class PostsController {
+
+  constructor(private postService: PostsService) {}
+
+  @Post()
+  createPost(@Body() dto: CreatePostDto) {
+
+  }
+}
